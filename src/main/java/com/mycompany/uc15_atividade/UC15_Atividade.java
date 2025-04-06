0fd575916ab7c186d1d6c632163061cf1f8a0d71
cleonirs <cleonirs@gmail.com>
2025-04-06 09:42:55 -0300

    Commit #2 Teste de Atualização

--- src/main/java/com/mycompany/uc15_atividade/UC15_Atividade.java
+++ src/main/java/com/mycompany/uc15_atividade/UC15_Atividade.java
@@ -4,7 +4,7 @@ import com.mycompany.uc15_atividade.persistencia.JPAUtil;
 
 /**
  *
- * @author cleonirlopes
+ * @author cleonirlopes Inclusao de comentario no GIT
  */
 public class UC15_Atividade {
 
